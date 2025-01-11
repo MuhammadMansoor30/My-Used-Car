683d9f529f715e10ee978f3df6eab96a9a865a00
MuhammadMansoor30 <mm1704682@gmail.com>
2025-01-11 12:39:44 +0500

    Unit Testing Complete

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -9,9 +9,17 @@ describe("Auth Service", () => {
     let dummyUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
+        const users: User[] = [];   // Craeting users array of Type User Entity Array
         dummyUsersService = {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User), 
+            find: (eamil: string) => {
+                const filrerredUsers = users.filter(users => users.email === eamil);
+                return Promise.resolve(filrerredUsers);   // Returning Promise as real USerService return Promise.
+            },
+            create: (email: string, password: string) => {
+                const user = ({id: Math.floor(Math.random() * 99999), email, password} as User);
+                users.push(user)
+                return Promise.resolve(user);
+            }, 
         };
     
         const module = await Test.createTestingModule({
@@ -43,10 +51,13 @@ describe("Auth Service", () => {
     });
 
     it('signups using a email which is already in use', async () => {
-        dummyUsersService.find = () => Promise.resolve([{id: 1, email: 'abc@email.com', password: 'abcd'} as User]);
-        // Updating the find method so that we can check that user already exists.
+        // dummyUsersService.find = () => Promise.resolve([{id: 1, email: 'abc@email.com', password: 'abcd'} as User]);
+        // Updating the find method so that we can check that user already exists. 
+        // Same reason to comment as in next two tests
+
+        await authService.signup('test@email.com', 'abcde');
         await expect(
-            authService.signin('abc@email.com', 'abcd'),
+            authService.signup('test@email.com', 'abcde'),
         ).rejects.toThrow(BadRequestException); 
 
         // Testing and throwing bad request exception is user signups with an existing email.
@@ -59,23 +70,26 @@ describe("Auth Service", () => {
     });
 
     it("throws error if user signsin with an incorrect password" , async () => {
-        dummyUsersService.find = () => Promise.resolve([{id: 1, email: "abc@email.com", password: 'pass'} as User]);
+        // dummyUsersService.find = () => Promise.resolve([{id: 1, email: "abc@email.com", password: 'pass'} as User]);
         // Find method always returns the user which we have provided above as in testing we will use dummy data.
+        // We dont need to re create this find method again and again as we have created an implementation of it above
+
+        await authService.signup("abc@email.com", 'abcde');
         await expect(
             authService.signin("abc@email.com", 'abcd'),
         ).rejects.toThrow(BadRequestException);
     });
 
     it('Signs In a user if correct password is provided', async () => {
-        dummyUsersService.find = () => Promise.resolve([{id: 1, email: 'abc@email.com', 
-            password: '85c6fe95c00ddeb4.960428c9ba8e5361afdebdee7afb5657bad7fae54d82b8e3ad4237701c7bb588'} as User]);
+        // dummyUsersService.find = () => Promise.resolve([{id: 1, email: 'abc@email.com', 
+        //     password: '85c6fe95c00ddeb4.960428c9ba8e5361afdebdee7afb5657bad7fae54d82b8e3ad4237701c7bb588'} as User]);
+        // Using Array approach to call signup method from authService first
+
+        // Creating a new user to check the hash password and using that same password to sign the user in.
+        await authService.signup('abc@email.com', 'mypassword');
 
         const user = await authService.signin('abc@email.com', 'mypassword');
         expect(user).toBeDefined();
-
-        // Creating a new user to check the hash password and using that same password to sign the user in.
-        // const user = await authService.signup('abc@gmail.com', 'mypassword');
-        // console.log(user);
     });
 });
 
@@ -90,3 +104,4 @@ describe("Auth Service", () => {
 // In DI system the providers array is the list of services which are required by the Dependency Injection container.
 // Specifying the type of dummyUsersService to Partial<UsersService> so that we can tell typescript that we want the methods with same structure as in UsersService and Partial denotes that not all methods of UsersService are required.
 // Also specifying the type of create method as User since our UserEntity has some other function which we dont want to implement so by providing the type of User entity typescript can cover that for us.
+// Using a more better way to test the passwords feature using a Users entity array which will temporarily stores the users and check and match their passwords.
